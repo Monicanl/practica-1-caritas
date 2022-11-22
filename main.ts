c7e81d47e80c666404b80494af2e90a8af696600
Monicanl <118841496+Monicanl@users.noreply.github.com>
2022-11-22 18:12:43 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Angry)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("Hello!")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Sad)
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
 })
