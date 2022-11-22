f5b4e22da6dd4ed92fcd375f771b5d0dba54347b
Monicanl <118841496+Monicanl@users.noreply.github.com>
2022-11-22 18:09:24 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,15 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HAPPY)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_shake():
+    basic.show_icon(IconNames.ANGRY)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_ab():
+    basic.show_string("Hello!")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_icon(IconNames.SAD)
+input.on_button_pressed(Button.B, on_button_pressed_b)
