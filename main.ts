3bac738d3d5743f0071e434f77e74b83acc8e16e
Monicanl <118841496+Monicanl@users.noreply.github.com>
2022-11-23 17:49:01 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 75)
         } else {
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
             }
         }
     }
